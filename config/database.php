1bf674e4c565c9cbb9b84343b0a0c6579e080d15
Rescobarmo <rescobarmo@redsalud.cl>
2026-07-22 15:47:58 -0400

    fix: default DB_HOST to 127.0.0.1, use rsr_srv_rsr in docker-compose

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_HOST', getenv('DB_HOST') ?: 'rsr-srv-rsr-phpmyadmin.jdseuk.easypanel.host');
+define('DB_HOST', getenv('DB_HOST') ?: '127.0.0.1');
 define('DB_PORT', getenv('DB_PORT') ?: 3306);
 define('DB_NAME', getenv('DB_NAME') ?: 'marketing');
 define('DB_USER', getenv('DB_USER') ?: 'rsr');
